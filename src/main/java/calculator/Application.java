2b04ce26ebc0e6d787919fac381eb7b4fc975072
심호경 <ghrudtla@naver.com>
2025-10-18 00:47:52 +0900

    feat: 빈 문자열 입력 시 0 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해주세요");
+
+        try{
+            String input = Console.readLine();
+            int sum = StringAddCalculator.add(input);
+            System.out.println("결과 : "+sum);
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }catch(Exception e){
+            System.out.println("알 수 없는 오류가 발생했습니다.");
+        }
+
+
     }
 }
---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,18 @@
+package calculator;
+
+public class StringAddCalculator {
+    private StringAddCalculator(){
+
+    }
+    public static int add(String input){
+
+        //빈 문자열이면 0 반환
+        if(input==null||input.isEmpty()){
+            return 0;
+        }
+
+        //이후 단계(기분 구분자 처리)는 다음 단계에서 구현 예정
+        return 0;
+    }
+
+}
