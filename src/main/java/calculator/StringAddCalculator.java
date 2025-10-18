22b33999286b34013245c0c5e4253b2b6bc6f747
심호경 <ghrudtla@naver.com>
2025-10-18 18:26:34 +0900

    feat: 커스텀 구분자 파싱 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,9 +1,9 @@
 package calculator;
 
 public class StringAddCalculator {
-    private StringAddCalculator(){
+    private StringAddCalculator(){}
 
-    }
+
     public static int add(String input){
 
         //빈 문자열이면 0 반환
@@ -11,14 +11,25 @@ public class StringAddCalculator {
             return 0;
         }
 
-        String[] numbers = input.split("[,:]");
+        String delimiter = "[,:]"; //기본 구분자
+        String numbers = input;
 
-        int sum = 0;
-        for(String number:numbers){
-            sum+=Integer.parseInt(number); //아직 입력값 검증은 안함(음수,숫자 아님 등)
+        if(input.startsWith("//")){
+            int newlineIndex = input.indexOf("\n");
+            if(newlineIndex==-1){
+                throw new IllegalArgumentException("커스텀 구분자 형식이 올바르지 않습니다.");
+            }
+
+            //   "//"와 "\n" 사이의 문자를 구분자로 사용
+            delimiter = input.substring(2, newlineIndex);
+            numbers = input.substring(newlineIndex+1);
         }
 
-
+        String[] tokens = numbers.split(delimiter);
+        int sum=0;
+        for(String token:tokens){
+            sum+=Integer.parseInt(token);
+        }
         return sum;
     }
 
