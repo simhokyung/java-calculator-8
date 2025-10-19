7abd4175f5a9880a3cb98958db2de90ef3e98085
심호경 <ghrudtla@naver.com>
2025-10-19 22:01:29 +0900

    refactor: 불필요한 Exception 캐치 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,23 +2,19 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("덧셈할 문자열을 입력해주세요");
+        System.out.println("덧셈할 문자열을 입력해 주세요");
 
-        try{
+        try {
             String input = Console.readLine();
             int sum = StringAddCalculator.add(input);
-            System.out.println("결과 : "+sum);
-        }catch(IllegalArgumentException e){
+            System.out.println("결과 : " + sum);
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-        }catch(Exception e){
-            System.out.println("알 수 없는 오류가 발생했습니다.");
+            return;//명시적으로 종료 의도 표기
         }
 
 
