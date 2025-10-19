85fc2cf072bd1d6bd8c02d88a01eb24d1557d723
심호경 <ghrudtla@naver.com>
2025-10-20 06:01:39 +0900

    fix: 예외를 catch하지 않고 IllegalArgumentException을 전파하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,15 +7,11 @@ public class Application {
     public static void main(String[] args) {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
 
-        try {
-            String input = Console.readLine();
-            int sum = StringAddCalculator.add(input);
-            System.out.println("결과 : " + sum);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return;//명시적으로 종료 의도 표기
-        }
+        // 예외를 잡아 출력하지 말 것! (채점기는 예외 전파/종료를 기대)
+        int sum = StringAddCalculator.add(input);
+        System.out.println("결과 : " + sum);
 
 
     }
